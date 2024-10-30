c442b6858771f7e3b0fd273daeb914e9bc3a4e3f
tim <pr3roxdlc.yt@web.de>
2024-10-30 09:31:31 +0100

    Aufgabe 1

---
+++ CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/tim/compgraf/asteroids
+# Build directory: /home/tim/compgraf/asteroids
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
